5e31ff219729a5223b4f5272266f5fc7123c4164
Warren Paschetto <wdpaschetto@gmail.com>
2024-10-03 18:30:08 -0400

    fixed dark mode role buttons on small header

--- src/components/layout/headers/SmallScreenHeader.tsx
+++ src/components/layout/headers/SmallScreenHeader.tsx
@@ -4,7 +4,7 @@ import { useGlobalState } from '@/context';
 import Image from 'next/image';
 
 export default function SmallScreenHeader() {
-  const { toggleRole } = useGlobalState();
+  const { role, toggleRole } = useGlobalState();
 
   return (
     <div className="flex flex-col bg-white dark:bg-gray-900">
@@ -20,14 +20,18 @@ export default function SmallScreenHeader() {
 
       <div className="flex flex-row justify-center mt-3">
         <h1
-          className="text-accent dark:text-white text-center text-xl hover:text-[#FCA311] dark:hover:text-[#FCA311] font-bold cursor-pointer border-2 p-2 rounded-full border-accent bg-blue-100"
+          className={role ==="developer" ? "text-hover-light text-center text-xl font-bold cursor-pointer border-2 p-2 rounded-full border-accent bg-blue-100 dark:bg-secondary-dark dark:border-blue-100"
+            :
+            "text-accent dark:text-[#F2E9E4] text-center text-xl hover:text-[#FCA311] dark:hover:text-[#FCA311] font-bold cursor-pointer border-2 p-2 rounded-full border-accent bg-blue-100 dark:bg-secondary-dark dark:border-blue-100"}
           onClick={() => toggleRole('developer')}
         >
          Web Developer
         </h1>
         <h1 className="text-neutral-900 dark:text-white text-center text-xl font-bold">&nbsp;/&nbsp;</h1>
         <h1
-          className="text-accent dark:text-white text-center text-xl hover:text-[#FCA311] dark:hover:text-[#FCA311] font-bold cursor-pointer border-2 p-2 rounded-full border-accent bg-blue-100"
+           className={role ==="trainer" ? "text-hover-light text-center text-xl font-bold cursor-pointer border-2 p-2 rounded-full border-accent bg-blue-100 dark:bg-secondary-dark dark:border-blue-100"
+            :
+            "text-accent dark:text-[#F2E9E4] text-center text-xl hover:text-[#FCA311] dark:hover:text-[#FCA311] font-bold cursor-pointer border-2 p-2 rounded-full border-accent bg-blue-100 dark:bg-secondary-dark dark:border-blue-100"}
           onClick={() => toggleRole('trainer')}
         >
            Personal Trainer
