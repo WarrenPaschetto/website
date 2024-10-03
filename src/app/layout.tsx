f09e5b9058883e5b3108fc531905efdd775b582e
Warren Paschetto <wdpaschetto@gmail.com>
2024-10-03 15:00:52 -0400

    made changes to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,21 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { GlobalStateProvider } from "@/context";
 
-//const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Top Personal Trainer & Web Developer | Pendleton, North Tonawanda, Clarence, Amherst, NY',
+  title: 'Personal Trainer & Web Developer | Pendleton, North Tonawanda, Clarence, Amherst, NY',
   description:
-    'Top-rated personal trainer specializing in weight loss, strength training, muscle building, nutrition, and injury prevention in Pendleton, NY, North Tonawanda, NY, Clarence, NY, and Amherst, NY. Also offering web development services for web apps and websites.',
-  keywords: 'personal trainer, weight loss, strength training, muscle building, nutrition, injury prevention, web developer, web apps, websites, Pendleton, North Tonawanda, Clarence, Amherst, NY',
+    'Top-rated NSCA and CSCS certified personal trainer specializing in weight loss, strength training, muscle building, nutrition, and injury prevention in Pendleton, NY, North Tonawanda, NY, Clarence, NY, and Amherst, NY. Also offering web development services for web apps and websites.',
+  keywords: 'personal trainer, weight loss, strength training, muscle building, nutrition, injury prevention, MS Health and Human Performance, NSCA-CPT, CSCS, web developer, web apps, websites, Pendleton, North Tonawanda, Clarence, Amherst, NY',
   authors: [{ name: 'Warren Paschetto, MS, CSCS*D, NSCA-CPT*D' }],
   openGraph: {
     title: 'Top Personal Trainer & Web Developer | Pendleton, North Tonawanda, Clarence, Amherst, NY',
     description:
       'Looking for the best personal trainer in Pendleton, NY, North Tonawanda, Clarence, or Amherst? I specialize in weight loss, strength training, and nutrition. Also offering web development services for websites and web apps.',
     url: 'https://warrenpaschetto.com',
-    siteName: 'Your Personal Trainer & Web Developer',
-    /*images: [
-      {
-        url: 'https://yourwebsite.com/og-image.jpg', // Replace with your Open Graph image
-        width: 1200,
-        height: 630,
-        alt: 'Personal Trainer and Web Developer in Pendleton and surrounding areas',
-      },
-    ],*/
+    siteName: 'Personal Trainer & Web Developer',
     locale: 'en_US',
     type: 'website',
   },
