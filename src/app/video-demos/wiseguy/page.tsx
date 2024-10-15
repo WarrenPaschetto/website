73a81eb45c3225e7cc5e907c97e6f1f22118db9b
Warren Paschetto <wdpaschetto@gmail.com>
2024-10-15 10:15:42 -0400

    fixed video frame size

--- src/app/video-demos/wiseguy/page.tsx
+++ src/app/video-demos/wiseguy/page.tsx
@@ -5,9 +5,9 @@ import React from 'react';
 const WiseguyVideo = () => {
   return (
     <main className="relative bg-white dark:bg-gray-900">
-      <div className="h-[80%] mt-28 flex flex-col w-full p-4">
+      <div className="h-auto mt-28 flex flex-col w-full p-4">
         <h1 className="text-accent dark:text-text-primary-dark font-bold text-center text-4xl m-10">Wiseguy App Video</h1>
-        <div className="w-[90%] md:w-[70%] mx-auto m-10  p-4">
+        <div className="w-[90%] md:w-[70%] mx-auto m-10 p-4">
           <p
             className="text-xl mx-auto text-accent dark:text-text-primary-dark"
           >
@@ -15,8 +15,8 @@ const WiseguyVideo = () => {
             to the public because of the cost of tokens each time it is run. All the code is available on&nbsp;
             <a
             href="https://github.com/WarrenPaschetto/wiseguy-siri.git"
-            target="_blank" // Opens link in a new tab
-            rel="noopener noreferrer" // Security feature to prevent potential vulnerabilities
+            target="_blank"
+            rel="noopener noreferrer"
             className="text-blue-600 underline dark:text-blue-400"
           >
             GitHub
@@ -24,16 +24,17 @@ const WiseguyVideo = () => {
             .
           </p>
         </div>
-        <div className="w-[90%] md:w-[70%] mx-auto m-10  p-4">
+        <div className="w-[90%] md:w-[70%] mx-auto m-10 p-4">
+          <div className="relative pb-[56.25%] h-0 overflow-hidden">
             <iframe
-              width="100%"
-              height="100%"
+              className="absolute top-0 left-0 w-full h-full"
               src="https://www.youtube.com/embed/Dvllxk4jTs0"
               title="Wiseguy App Video"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
+          </div>
         </div>
       </div>
     </main>
